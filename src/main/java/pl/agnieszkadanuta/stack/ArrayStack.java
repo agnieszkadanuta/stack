1bbaf9a620c890efcad52373f9f03adde900d88b
Agnieszka Chudzicka <agnieszkadanuta.chudzicka@gmail.com>
2018-04-03 19:03:48 +0200

    add Tests

--- src/main/java/pl/agnieszkadanuta/stack/ArrayStack.java
+++ src/main/java/pl/agnieszkadanuta/stack/ArrayStack.java
@@ -1,19 +1,46 @@
 package pl.agnieszkadanuta.stack;
 
+import java.util.Arrays;
+
 public class ArrayStack<E> implements Stack<E> {
 
-    @Override
-    public void push(E element) {
+    //początkowy rozmiar stosu, który jest stałą
+    private static final int DEFAULT_SIZE = 10;
 
+    //tablica typu E
+    private Object[] stackTab;
+
+    //licznik elementów w tablicy
+    private int countElements;
+
+    //konstruktor tworzący tablicę
+    public ArrayStack() {
+       stackTab = new Object[DEFAULT_SIZE];
+       countElements = 0;
     }
 
     @Override
+    public void push(E element) {
+        if(countElements == stackTab.length) {
+            stackTab = Arrays.copyOf(stackTab, 2 * stackTab.length);
+        }
+        stackTab[countElements] = element;
+        countElements++;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
     public E pop() {
-        return null;
+        if(countElements > 0){
+            countElements--;
+            return (E)stackTab[countElements];
+        }else{
+            throw new IllegalStateException("Stack is empty");
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return countElements == 0;
     }
 }
--- src/test/java/pl/agnieszkadanuta/stack/ArrayStackTest.java
+++ src/test/java/pl/agnieszkadanuta/stack/ArrayStackTest.java
@@ -1,5 +1,7 @@
 package pl.agnieszkadanuta.stack;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +9,51 @@ import static org.junit.jupiter.api.Assertions.*;
 class ArrayStackTest {
 
     @Test
+    @DisplayName("created stack should be empty")
+
+    void createStack(){
+
+        Stack<String> stack = new ArrayStack<>();
+        assertTrue(stack.isEmpty());
+    }
+
+    @Test
+    @DisplayName("should enlarge stack")
+    void enlargeStack(){
+        Stack<Integer> stack = new ArrayStack<>();
+        //push w pętli dużo razy
+        int size = 100;
+        for(int i = 0; i < size; i++){
+            stack.push(i);
+        }
+
+
+        //pop w pętli dużo razy
+        for(int i = 0 ; i < size; i++){
+            Integer element = stack.pop();
+            assertEquals(size - i - 1, element.intValue());
+        }
+        //każdy zdjęty powinien być taki sam
+
+        assertTrue(stack.isEmpty());
+
+        //stos powinien być pusty
+    }
+
+
+    @Test
+    @DisplayName("should contain one element after adding element to empty stack")
     void push() {
+
+        Stack<String> stack = new ArrayStack<>();
+        String element = "element";
+        stack.push(element);
+
+        String actualElement = stack.pop();
+
+        assertEquals(element, actualElement);
+        assertTrue(stack.isEmpty());
+
     }
 
     @Test
